fb8620850ad56ef98715a33a454380060152cc3c
Mihir867 <mihirkantthakur867@gmail.com>
2024-11-01 18:04:07 +0530

    animated added

--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -6,7 +6,7 @@ function Blog() {
       <div className="image">
        <img className="img" src = "images/Asthetic Image.jpeg"></img>
        <div className='text'></div>
-       <p className='text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, aliquam culpa? Aperiam impedit vero ratione 
+       <p className='text-center pt-10 animate-bounce animate-delay-300'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, aliquam culpa? Aperiam impedit vero ratione 
           perspiciatis distinctio adipisci aliquam expedita odio, iure magnam laudantium nihil modi beatae earum 
           \laborum id?</p>
           </div>
@@ -14,7 +14,7 @@ function Blog() {
           <div className="image">
        
        <div className='text'></div>
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, aliquam culpa? Aperiam impedit vero ratione 
+       <p className='pt-20'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, aliquam culpa? Aperiam impedit vero ratione 
           perspiciatis distinctio adipisci aliquam expedita odio, iure magnam laudantium nihil modi beatae earum 
           \laborum id?</p>
           <img className="img" src = "images/Asthetic Image.jpeg"></img>
@@ -23,7 +23,7 @@ function Blog() {
           <div className="image">
        <img className="img" src = "images/Asthetic Image.jpeg"></img>
        <div className='text'></div>
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, aliquam culpa? Aperiam impedit vero ratione 
+       <p className='pt-20'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, aliquam culpa? Aperiam impedit vero ratione 
           perspiciatis distinctio adipisci aliquam expedita odio, iure magnam laudantium nihil modi beatae earum 
           \laborum id?</p>
           </div>
