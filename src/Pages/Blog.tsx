2c716fc9289ce7c7b8e65a177ba361bad66d83db
Mihir867 <mihirkantthakur867@gmail.com>
2024-11-01 17:50:03 +0530

    added flex

--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -2,32 +2,34 @@
 import './Blog.css';
 function Blog() {
     return (
-      <div className="fullHeightContainer">
+      <div className="fullHeightContainer flex flex-col text-center ">
       <div className="image">
+       <img className="img" src = "images/Asthetic Image.jpeg"></img>
+       <div className='text'></div>
+       <p className='text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, aliquam culpa? Aperiam impedit vero ratione 
+          perspiciatis distinctio adipisci aliquam expedita odio, iure magnam laudantium nihil modi beatae earum 
+          \laborum id?</p>
+          </div>
+
+          <div className="image">
+       
+       <div className='text'></div>
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, aliquam culpa? Aperiam impedit vero ratione 
+          perspiciatis distinctio adipisci aliquam expedita odio, iure magnam laudantium nihil modi beatae earum 
+          \laborum id?</p>
+          <img className="img" src = "images/Asthetic Image.jpeg"></img>
+          </div>
+
+          <div className="image">
        <img className="img" src = "images/Asthetic Image.jpeg"></img>
        <div className='text'></div>
        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, aliquam culpa? Aperiam impedit vero ratione 
           perspiciatis distinctio adipisci aliquam expedita odio, iure magnam laudantium nihil modi beatae earum 
           \laborum id?</p>
-          
-          <div className="image1">
-       <img className="img1" src = "images/Passion Image.jpeg"></img>
-       <div className='text'></div>
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, aliquam culpa? Aperiam impedit vero ratione 
-          perspiciatis distinctio adipisci aliquam expedita odio, iure magnam laudantium nihil modi beatae earum 
-          \laborum id?</p>
+          </div>
+         
       </div>
-
-      <div className="image2">
-       <img className="img2" src = "images/Asthetic Image.jpeg"></img>
-       <div className='text'></div>
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, aliquam culpa? Aperiam impedit vero ratione 
-          perspiciatis distinctio adipisci aliquam expedita odio, iure magnam laudantium nihil modi beatae earum 
-          \laborum id?</p>
-
-      </div>
-      </div>
-      </div>
+    
     );
   }
   
